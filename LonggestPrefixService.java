8c65457ca655e0b6a3199c8810cc9968f01aea56
May Chi <116498227+maychilo@users.noreply.github.com>
2022-10-28 05:05:45 +1100

    Add files via upload

---
+++ LonggestPrefixService.java
@@ -0,0 +1,31 @@
+package homeworkD8;
+
+public class LonggestPrefixService {
+	public static String longestPrefix(String[] strs) {
+
+		String prefix = strs[0].substring(0, minLength(strs)); // 1
+
+		   int i = 1;
+
+			while (!strs[i].contains(prefix) && i< strs.length) { //length(strs)
+
+				prefix = prefix.substring(0, prefix.length() - 1);//minlenght(strs)
+
+				if (prefix.length() == 0)
+					return "";//1
+			}
+
+		return prefix; //1
+		
+	}// Time complexity O(n) = length(strs)+minlenght(strs)+3
+
+	public static int minLength(String[] strs) {
+		int minLength = strs[0].length();
+		for (int i = 1; i < strs.length; i++) {
+			if (strs[i].length() < minLength) {
+				minLength = strs[i].length();
+			}
+		}
+		return minLength;
+	}
+}
