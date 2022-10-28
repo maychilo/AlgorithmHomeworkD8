faccba453b4f1241c83baa7d4c21e04da30b1f57
May Chi <116498227+maychilo@users.noreply.github.com>
2022-10-28 15:28:25 +1100

    Update LonggestPrefixService.java

--- LonggestPrefixService.java
+++ LonggestPrefixService.java
@@ -5,19 +5,20 @@ public class LonggestPrefixService {
 
 		String prefix = strs[0].substring(0, minLength(strs)); // 1
 
-		   int i = 1;
+		   for (int j = 0; j < strs.length; j++) { // length(strs)
 
-			while (!strs[i].contains(prefix) && i< strs.length) { //length(strs)
+			while (!strs[j].contains(prefix)) { 
 
-				prefix = prefix.substring(0, prefix.length() - 1);//minlenght(strs)
+				prefix = prefix.substring(0, prefix.length() - 1);// minlenght(strs)
 
 				if (prefix.length() == 0)
-					return "";//1
+					return "";// 1
 			}
+		}
 
 		return prefix; //1
 		
-	}// Time complexity O(n) = length(strs)+minlenght(strs)+3
+	}// Time complexity O(n) = length(strs)*minlenght(strs)+3
 
 	public static int minLength(String[] strs) {
 		int minLength = strs[0].length();
